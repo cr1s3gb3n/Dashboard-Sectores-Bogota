256aec688ea793bd0b13eb5dff3087fc505ee607
Cristian David Segura Benavides <c.segurab@uniandes.edu.co>
2025-11-26 10:12:26 -0500

    Versión completa funcional con panel coroplético y mejoras UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,19 @@ Promise.all([
   inicializarPanel();
   iniciarAnimacion();
 }).catch(err => console.error("❌", err));
+
+// Desplegable de indicadores
+document.getElementById("toggle-indicadores").addEventListener("click", () => {
+  const wrapper = document.getElementById("indicadores-wrapper");
+  const arrow = document.getElementById("toggle-indicadores");
+
+  if (wrapper.classList.contains("expanded")) {
+    wrapper.classList.remove("expanded");
+    wrapper.classList.add("collapsed");
+    arrow.classList.remove("rotated");
+  } else {
+    wrapper.classList.remove("collapsed");
+    wrapper.classList.add("expanded");
+    arrow.classList.add("rotated");
+  }
+});
